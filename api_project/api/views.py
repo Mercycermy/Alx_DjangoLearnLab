f7595e3015ef9e8b267e42159731f80705d48931
Mercycermy <mercycermy5@gmail.com>
2025-03-07 04:26:04 -1200

    add from .serializers import BookSerializer

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from  .serializers import BookSerializer
+from .serializers import BookSerializer
 from .models import Book
 from rest_framework import generics
 
