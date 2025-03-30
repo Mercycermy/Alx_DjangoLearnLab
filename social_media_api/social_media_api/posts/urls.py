809a17ab95ae3c98708346e5659cad0026c2a7e7
Mercycermy <mercycermy5@gmail.com>
2025-03-30 02:33:49 -1200

    adding account and notification

--- social_media_api/social_media_api/posts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostListCreateAPIView, RegisterView
-
-urlpatterns = [
-    path('posts/', PostListCreateAPIView.as_view(), name='post-list-create'),
-    path('register/', RegisterView.as_view(), name='register'),
-    #path('api/', include('social_media_api.urls')),
-]
